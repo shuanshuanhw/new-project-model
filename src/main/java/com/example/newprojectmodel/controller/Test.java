b0513cc847a54a4b4a1acdfde25493b4a759df94
shuanshuan <178650140@qq.com>
2022-04-07 10:28:40 +0800

    20220407-1

--- src/main/java/com/example/newprojectmodel/controller/Test.java
+++ src/main/java/com/example/newprojectmodel/controller/Test.java
@@ -3,6 +3,7 @@ package com.example.newprojectmodel.controller;
 import com.example.newprojectmodel.annotation.Log;
 import com.example.newprojectmodel.entity.SysUser;
 import com.example.newprojectmodel.enums.BusinessType;
+import org.apache.catalina.session.StandardSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -17,9 +20,11 @@ public class Test {
 
     @GetMapping("/test")
     @Log(title = "测试",businessType = BusinessType.OTHER)
-    public String test()
+    public String test(HttpServletRequest req)
     {
+
         return "test";
+
     }
 
     @PostMapping("/test")
